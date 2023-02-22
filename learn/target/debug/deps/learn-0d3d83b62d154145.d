8301a21d12516b9751dbb074f6c1af4fcc599cfb
Nikita Voloboev <nikita.voloboev@gmail.com>
2023-02-22 18:57:37 +0100

    .

---
+++ learn/target/debug/deps/learn-0d3d83b62d154145.d
@@ -0,0 +1,5 @@
+/Users/nikiv/src/lib/rust/learn/target/debug/deps/learn-0d3d83b62d154145.rmeta: src/main.rs
+
+/Users/nikiv/src/lib/rust/learn/target/debug/deps/learn-0d3d83b62d154145.d: src/main.rs
+
+src/main.rs:
---
+++ learn/target/debug/deps/learn-e28d1cab2e9aa1d9.d
@@ -0,0 +1,5 @@
+/Users/nikiv/src/lib/rust/learn/target/debug/deps/learn-e28d1cab2e9aa1d9.rmeta: src/main.rs
+
+/Users/nikiv/src/lib/rust/learn/target/debug/deps/learn-e28d1cab2e9aa1d9.d: src/main.rs
+
+src/main.rs:
