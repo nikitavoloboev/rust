f3a37149023743ad0c89fe7fe8a7ac877c76f8f2
Nikita <nikita.voloboev@gmail.com>
2024-05-09 11:57:24 +0200

    .

--- cli/process/src/main.rs
+++
@@ -1,7 +0,0 @@
-// TODO: update readme of package
-use log_macro::log;
-
-fn main() {
-    let a = rust_utils::add(2, 2);
-    log!(a);
-}
--- try/src/main.rs
+++ try/src/main.rs
@@ -1,7 +1,25 @@
-#![allow(dead_code)]
+// #![allow(dead_code)]
 use log_macro::log;
 
+struct User {
+    username: String,
+    email: String,
+    signed_in_count: u64,
+    active: bool,
+}
+
 fn main() {
-    let animals = vec!["dog"];
-    log!(animals);
+    structs();
+    log!("done");
+}
+
+fn structs() {
+    let mut user1 = User {
+        email: String::from("nikita@nikiv.dev"),
+        username: String::from("nikiv"),
+        active: true,
+        signed_in_count: 1,
+    };
+    let name: String = user1.username;
+    user1.username = String::from("nikivi");
 }
